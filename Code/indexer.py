a03c7c0e48cccf0dc9b4dabe4c957d04365909db
Karina <karina.palyutina@hotmail.com>
2012-11-18 17:25:15 +0000

    Still no worky for Web dir!

--- Code/indexer.py
+++ Code/indexer.py
@@ -1,14 +1,14 @@
-from whoosh.fields import Schema, TEXT, ID
+from whoosh.fields import Schema, TEXT, ID, STORED
 import os.path
 from whoosh.index import create_in, open_dir
 from whoosh.query import *
 from BeautifulSoup import BeautifulSoup
 from whoosh.qparser import QueryParser
 from whoosh.searching import Searcher
+from whoosh.filedb.filestore import FileStorage
 
 
 #if __name__ == "__main__":
-    
 
 def visible(element):
     if element.parent.name in ['style', 'script', '[document]', 'head']:
@@ -21,42 +21,52 @@ def visible(element):
      #   return False
     return True
 
-def index(folder,query):
-    schema = Schema(title=TEXT(stored=True), content=TEXT, path=ID(stored=True))
+
+def getSchema():
+    return Schema(title=TEXT(stored=True),content=TEXT,path=ID(unique=True,stored=True),time=STORED)
+
+def add_doc(writer,path):
+    file = open(path,'r')
+    data = file.read()
+    file.close()
+    soup = BeautifulSoup(data)
+    if (soup.title):
+        title = unicode(soup.title.string)
+    else:
+        components = path.split(u'/')
+        last = len(components)-1
+        title = components[last]
+    content = unicode(soup.getText(u' '))
+    #texts = filter(visible,soup.body.findAll(text=True))
+    mtime = os.path.getmtime(path)
+    writer.add_document(title=title,content=content,path=path,time=mtime)
+
+def traverse(dir):
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            yield root+u'/'+f
+
+
+def index(dir):
+    schema = getSchema()
     if not os.path.exists("index"):
         os.mkdir("index")
-
-    ix = create_in("index",schema)
-    writer = ix.writer()
-    
-    for filename in os.listdir(folder):
-       # if os.path.isdir(folder+'/'+filename):
-        #    print filename +"is a directory" 
-        data = open(folder+filename,'r').read()
-        soup = BeautifulSoup(data)
-        if (soup.title):
-            myTitle = soup.title.string
-        #texts = filter(visible,soup.getText(' '))
-        myContent = soup.getText(u' ')
-        myPath = folder+filename
-
-        writer.add_document(title=myTitle,content=myContent,
-                path=myPath)
+    store = FileStorage("index")
+    ix = store.create_index(schema)
+    writer = ix.writer() #TODO:use with
+    for filename in traverse(dir):
+        add_doc(writer,filename)
     writer.commit()
 
 
 def search(query):
-    ix = open_dir("index")
+    store = FileStorage("index")
+    ix = store.open_index()
     qp = QueryParser("content",schema=ix.schema)
     q = qp.parse(query)
     with ix.searcher() as s:
         results = s.search(q)
         for doc in results:
             print doc
-
-
-        #print str(title) + str(path)#  + str(content)
-
-
 
 
