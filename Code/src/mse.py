38b25fac45c96cfe797ec2fa9c6bd09abd3f6488
Karina <karina.palyutina@hotmail.com>
2013-02-10 14:45:15 +0000

    Never needed to be a class in the first place.

--- Code/src/mse.py
+++ Code/src/mse.py
@@ -1,12 +1,6 @@
-import numpy as np
-import svm
+from numpy import array, sqrt, sum
 
-class MSE:
-
-    def __init__(self,data,f):
-        self.act = np.array(data[1])
-        self.pred = (map(lambda x: x[0,0],(map(f,data[0]))))
-
-    @property
-    def MSE(self):
-        return np.sqrt(np.sum((self.act-self.pred)**2)/len(self.act))
+def mse(X, Y, f):
+    pred = (map(lambda x: x[0,0],(map(f,X))))
+    act = array(Y)
+    return sqrt(sum((act-pred)**2)/len(act))
