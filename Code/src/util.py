b16774e30e41367b040fed6ee9823718d1b77058
Karina <karina.palyutina@hotmail.com>
2013-04-20 22:10:35 +0100

    appendices"

--- Code/src/util.py
+++ Code/src/util.py
@@ -2,16 +2,21 @@ from bs4 import BeautifulSoup as BS
 from nltk import PorterStemmer as PS
 
 def strip_punct(s):
-    """Take a string and return a unicode string with no punctuation symbols.
+    """Take a string and return a unicode string 
+    	with no punctuation symbols.
+
     >>> strip_punct(u'remove.the.dots')
     u'removethedots'
+
     >>> strip_punct(u'a very &confusing* line$')
     u'a very &confusing line$'
+
     >>> strip_punct('a (non)-unicode/ +string!!!')
     Traceback (most recent call last):
         ...
     TypeError: s must be unicode
     """
+
     if not isinstance(s,unicode):
        raise TypeError('s must be unicode')
     punctuation = u'!"#\'()*+,-./:;<=>?@[\]^_`{|}~'
@@ -19,9 +24,12 @@ def strip_punct(s):
     return s.translate(table)
 
 def clean(path):
-    """Take a page path and return the text on the page with no punctuation.     
+    """Take a page path and return the text on 
+    	the page with no punctuation.     
+
     >>> clean('example_page.html')
-    [u'An', u'example', u'webpage', u'This', u'is', u'text', u'line', u'1', u'This', u'is', u'text', u'line', u'2']
+    [u'Text', u'line', u'1', u'Text', u'line', u'2']
+
     >>> clean('example_no_text.html')
     []
     """
@@ -33,14 +41,18 @@ def clean(path):
     return clean
 
 def has_image(path):
-    """Take a page and count number of images.
+    """Take a page path and return image count.
+
     >>> has_image('example_page.html')
     1
+
     >>> has_image('example_no_text.html')
     0
+
     >>> has_image('3images.html')
     3
     """
+
     with open(path,'r') as f:
         data = f.read()
     soup = BS(data,"lxml")
