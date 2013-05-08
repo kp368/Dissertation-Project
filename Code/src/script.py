779879627e9a0086312a19283ba2dd75ef99a17c
Karina <karina.palyutina@hotmail.com>
2013-05-08 23:44:19 +0100

    minor

--- Code/src/script.py
+++ Code/src/script.py
@@ -17,3 +17,27 @@ def data():
 def plt(l,t):
     f = svm.solve(l,svm.comb_prod)
     plot.plot(f,l.XY,t.XY)
+
+def fit(h):
+    l,t = data()
+    if h=='step':
+	S = svm.SVM(l.XY[0],l.get_XY(lambda x,y:x>=4)[1],0)
+	S.sample()
+	S.solve(svm.gauss,0.25)
+	f = S.predict
+        plot.plot_hyper(f,l.get_XY(lambda x,y: x>=4),t.get_XY(lambda x,y:x>=4))
+
+    elif h=='step_bad':
+	S = svm.SVM(l.XY[0],l.get_XY(lambda x,y:x>=4)[1],0)
+	S.sample()
+	S.solve(svm.gauss,0.15)
+	f = S.predict
+        plot.plot_hyper(f,l.get_XY(lambda x,y: x>=4),t.get_XY(lambda x,y:x>=4))
+    elif h=='poly':
+        S = svm.SVM(l.XY[0],l.get_XY(lambda x,y:x**2+y**2)[1],0);S.sample();S.solve(svm.poly,2); f = S.predict
+        plot.plot_hyper(f,l.get_XY(lambda x,y:x**2+y**2),t.get_XY(lambda x,y:x**2+y**2))
+
+    elif h =='lin':
+	S = svm.SVM(l.XY[0],l.get_XY(lambda x,y:x+y)[1],0);S.sample();S.solve(svm.lin); f = S.predict
+        plot.plot_hyper(f,l.get_XY(lambda x,y:x+y),t.get_XY(lambda x,y:x+y))
+
